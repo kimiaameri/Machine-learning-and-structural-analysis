5bd9626170fb22626bc954c866270d2de2ed77f8
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-07-23 15:52:31 -0500

    Update pythonBwa.py

--- pythonBwa.py
+++ pythonBwa.py
@@ -16,13 +16,17 @@ with open(outputFile,'w') as outFile:
     # Maximum memory required per CPU (in megabytes')
     outFile.write('#SBATCH --job-name=BWA \n')
     outFile.write('#SBATCH --error=BWA.%J.err \n')
-    outFile.write('#SBATCH --output=BWA.%J.out \n')  
+    outFile.write('#SBATCH --output=BWA.%J.out \n') 
+    outFile.write('cd $WORK/SNP-outputs/trimmomatic/ \n')  
+    outFile.write('mkdir 1 \n')  
+    outFile.write('cp $WORK/SNP-outputs/trimmomatic/* $WORK/SNP-outputs/trimmomatic/1/ \n')  
     count =0
+
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if count !=0:
-                outFile.write(f'sed -E "s/^((@|\+)ERR[^.]+\.[^.]+)\.(1|2|3|4|5)/\1/" row[0]-R1.paired.fq > row[0]-R1.paired.fq\n')
-                outFile.write(f'sed -E "s/^((@|\+)ERR[^.]+\.[^.]+)\.(1|2|3|4|5)/\1/" row[0]-R2.paired.fq > row[0]-R2.paired.fq\n')
+                outFile.write(f'sed -E "s/^((@|\+)ERR[^.]+\.[^.]+)\.(1|2|3|4|5)/\\1/" row[0]-R1.paired.fq > row[0]-R1.paired.fq\n')
+                outFile.write(f'sed -E "s/^((@|\+)ERR[^.]+\.[^.]+)\.(1|2|3|4|5)/\\1/" row[0]-R2.paired.fq > row[0]-R2.paired.fq\n')
                 outFile.write(f'{minicondaBin}bwa mem $WORK/SNP_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa $WORK/SNP-outputs/trimmomatic/{row[0]}-R1.paired.fq $WORK/SNP-outputs/trimmomatic/{row[0]}-R2.paired.fq >$WORK/SNP-outputs/samfiles/{row[0]}.sam\n')
             count =count+1
