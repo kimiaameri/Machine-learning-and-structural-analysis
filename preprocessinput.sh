59542c0db163567da82808c2905ffb7f425f2560
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-07-25 17:41:39 -0500

    Update preprocessinput.sh

--- preprocessinput.sh
+++ preprocessinput.sh
@@ -1,6 +1,15 @@
+#!/bin/sh
+#SBATCH --job-name=SRAtoolkit
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=168:00:00
+#SBATCH --mem=10gb
+#SBATCH --output=SRAtoolkit.%J.out
+#SBATCH --error=SRAtoolkit.%J.err
+
 for fastq in *.fastq
 do
- awk 'BEGIN {FS = "\t" ; OFS = "\n"} {header = $0 ; getline seq ; getline qheader ; getline qseq ; if (length(seq) >8) {print header, seq, qheader, qseq}}' < $fastq > filtered_$fastq
+ awk 'BEGIN {FS = "\t" ; OFS = "\n"} {header = $0 ; getline seq ; getline qheader ; getline qseq ; if (length(seq) >20) {print header, seq, qheader, qseq}}' < $fastq > filtered_$fastq
 done
 
 find . -name "*.fastq" -size 0k -delete
