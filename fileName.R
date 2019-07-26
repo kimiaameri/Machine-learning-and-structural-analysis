864b7e1f65a77e9c78ade5c97cf5ce3d4eacd6ec
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-07-26 11:28:09 -0500

    Create fileName.R

---
+++ fileName.R
@@ -0,0 +1,20 @@
+argv <- commandArgs(trailingOnly = TRUE)
+InputPath <- argv[1]
+OutputFile <- argv[2]
+
+
+listdata<- list.files(InputPath)
+listdata<- gsub(pattern = "_1.fastq",replacement = "",listdata, perl = T)
+listdata<- gsub(pattern = "_2.fastq",replacement = "",listdata, perl = T)
+listdata<- gsub(pattern = "_3.fastq",replacement = "",listdata, perl = T)
+listdata<- gsub(pattern = "_4.fastq",replacement = "",listdata, perl = T)
+listdata<- gsub(pattern = "_5.fastq",replacement = "",listdata, perl = T)
+
+listdata<- unique(listdata)
+
+#inputFile<-as.matrix(listdata) 
+#c1<- as.matrix(paste(as.character(inputFile[,1]),"_1.fastq", sep=""))
+#c2<- as.matrix(paste(as.character(inputFile[,1]),"_2.fastq", sep=""))
+#inputFile<- cbind(inputFile, c1, c2)
+#colnames(inputFile)<- c("name","Forward reads", "Reverse Reads")
+write.csv(inputFile)
