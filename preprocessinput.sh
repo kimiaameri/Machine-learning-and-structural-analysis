00d1b38d42e9c18446c2d8aca1b49d914138ac0c
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-07-29 03:21:13 -0500

    Update preprocessinput.sh

--- preprocessinput.sh
+++ preprocessinput.sh
@@ -7,11 +7,13 @@
 #SBATCH --output=process.%J.out
 #SBATCH --error=process.%J.err
 
-for fastq in *.fastq
-do
- awk 'BEGIN {FS = "\t" ; OFS = "\n"} {header = $0 ; getline seq ; getline qheader ; getline qseq ; if (length(seq) >25) {print header, seq, qheader, qseq}}' < $fastq > filtered_$fastq
-done
+#for fastq in *.fastq
+#do
+ #awk 'BEGIN {FS = "\t" ; OFS = "\n"} {header = $0 ; getline seq ; getline qheader ; getline qseq ; if (length(seq) >25) {print header, seq, qheader, qseq}}' < $fastq > filtered_$fastq
+#done
 
 find . -name "*.fastq" -size 0k -delete
 
-
+for x in `cat unpaird.finallist.isolates.txt`; do 
+find . -name "filtered_x*"  -delete
+done
