8a89199cf6a50fa59679e409a52851669d31f4f5
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-07-31 17:50:07 -0500

    Update pythonBwa.py

--- pythonBwa.py
+++ pythonBwa.py
@@ -17,16 +17,17 @@ with open(outputFile,'w') as outFile:
     outFile.write('#SBATCH --job-name=BWA \n')
     outFile.write('#SBATCH --error=BWA.%J.err \n')
     outFile.write('#SBATCH --output=BWA.%J.out \n') 
-    outFile.write('cd $WORK/SNP-outputs/trimmomatic/ \n')  
-    outFile.write('mkdir 1 \n')  
-    outFile.write('cp $WORK/SNP-outputs/trimmomatic/* $WORK/SNP-outputs/trimmomatic/1/ \n')  
+    #outFile.write('cd $WORK/SNP-outputs/trimmomatic/ \n')  
+    #outFile.write('mkdir 1 \n')  
+    #outFile.write('cp $WORK/SNP-outputs/trimmomatic/* $WORK/SNP-outputs/trimmomatic/1/ \n')  
     count =0
 
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if count !=0:
-                outFile.write(f'sed -E "s/^((@|\+)*RR[^.]+\.[^.]+)\.(1|2|3|4|5)/\\1/" row[0]-R1.paired.fq > row[0]-R1.paired.fq\n')
-                outFile.write(f'sed -E "s/^((@|\+)*RR[^.]+\.[^.]+)\.(1|2|3|4|5)/\\1/" row[0]-R2.paired.fq > row[0]-R2.paired.fq\n')
-                outFile.write(f'{minicondaBin}bwa mem $WORK/SNP_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa $WORK/SNP-outputs/trimmomatic/{row[0]}-R1.paired.fq $WORK/SNP-outputs/trimmomatic/{row[0]}-R2.paired.fq >$WORK/SNP-outputs/samfiles/{row[0]}.sam\n')
+                 #sed 's/\(^[@|\+].*RR[0-9]*\.[0-9]*\)\.[0-9] \(.*\)/\1 \2/'
+                outFile.write(f' "sed 's/\(^[@|\+].*RR[0-9]*\.[0-9]*\)\.[0-9] \(.*\)/\\1 \\2/'" row[0]-R1.paired.fq > refined-row[0]-R1.paired.fq\n')
+                outFile.write(f' "sed 's/\(^[@|\+].*RR[0-9]*\.[0-9]*\)\.[0-9] \(.*\)/\\1 \\2/'" row[0]-R2.paired.fq > refined-row[0]-R2.paired.fq\n')
+                outFile.write(f'{minicondaBin}bwa mem $WORK/SNP_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa $WORK/SNP-outputs/trimmomatic/refined-{row[0]}-R1.paired.fq $WORK/SNP-outputs/trimmomatic/refined-{row[0]}-R2.paired.fq >$WORK/SNP-outputs/samfiles/{row[0]}.sam\n')
             count =count+1
