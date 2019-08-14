a7b35c8bc030fdca1ddcd479277a51c60845b4e9
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-14 17:02:25 -0500

    Update pythonVariantAnalysis.py

--- pythonVariantAnalysis.py
+++ pythonVariantAnalysis.py
@@ -1,14 +1,15 @@
 import csv
 import sys
 
-if len(sys.argv) < 4:
+if len(sys.argv) < 5:
     sys.stderr.write('No Input CSV file and miniconda path GITHUB_PATH\n')
     sys.exit(0)
     
 inputFile = sys.argv[1]
 minicondaBin = sys.argv[2]
 githubPath = sys.argv[3]
-outputFile = "trimmomatic.sh"  
+index= sys.argv[4]
+outputFile = append (index,"snp.sh" )
 with open(outputFile,'w') as outFile:
     outFile.write('#!/bin/sh \n')
     outFile.write('#SBATCH --time=100:00:00   # Run time in hh:mm:ss  \n')
