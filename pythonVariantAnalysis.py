e3612e34e990453c5ecbf5074e1f2543840f6f35
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-14 17:30:13 -0500

    Update pythonVariantAnalysis.py

--- pythonVariantAnalysis.py
+++ pythonVariantAnalysis.py
@@ -8,8 +8,9 @@ if len(sys.argv) < 5:
 inputFile = sys.argv[1]
 minicondaBin = sys.argv[2]
 githubPath = sys.argv[3]
-index= sys.argv[4]
-outputFile = append (index,"snp.sh" )
+n= sys.argv[4]
+index = n.split('.')[0][10:]
+outputFile ="snp{}.sh".format(index)
 with open(outputFile,'w') as outFile:
     outFile.write('#!/bin/sh \n')
     outFile.write('#SBATCH --time=100:00:00   # Run time in hh:mm:ss  \n')
@@ -28,7 +29,7 @@ with open(outputFile,'w') as outFile:
               outFile.write(f'export LengthReverse=$(( `cat length_filtered_{row[0]}_2.fastq.txt` ))  \n' )
               outFile.write("LengthReverse=${LengthReverse%.*} \n")
               outFile.write(f'{minicondaBin}trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SNP-outputs/trimmomatic/trimlog/{row[0]}.trimlog $WORK/SNP/data/filtered_{row[1]} $WORK/SNP/data/filtered_{row[2]} $WORK/SNP-outputs/trimmomatic/{row[0]}-R1.paired.fq $WORK/SNP-outputs/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/SNP-outputs/trimmomatic/{row[0]}-R2.paired.fq $WORK/SNP-outputs/trimmomatic/{row[0]}-R2.unpaired.fq SLIDINGWINDOW:4:15 MAXINFO:50:0.5 LEADING:3 TRAILING:3 MINLEN:$LengthReverse \n')
-              outFile.write(f' cd $WORK/SNP-ouputs/trimmomatic/\n' )
+              outFile.write(f' cd $WORK/SNP-outputs/trimmomatic/\n' )
               outFile.write(f'sed "s/\(^[@|\+].*RR[0-9]*\.[0-9]*\)\.[0-9] \(.*\)/\\1 \\2/" {row[0]}-R1.paired.fq > refined-{row[0]}-R1.paired.fq\n')
               outFile.write(f'sed "s/\(^[@|\+].*RR[0-9]*\.[0-9]*\)\.[0-9] \(.*\)/\\1 \\2/" {row[0]}-R2.paired.fq > refined-{row[0]}-R2.paired.fq\n')
               outFile.write(f'{minicondaBin}bwa mem $WORK/SNP_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa $WORK/SNP-outputs/trimmomatic/refined-{row[0]}-R1.paired.fq $WORK/SNP-outputs/trimmomatic/refined-{row[0]}-R2.paired.fq >$WORK/SNP-outputs/samfiles/{row[0]}.sam\n')
