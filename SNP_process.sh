7aac6422be05ff0965b7ba0af5a16c3b4c6297d2
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-14 17:29:20 -0500

    Create SNP_process.sh

---
+++ SNP_process.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#SBATCH --time=100:00:00   # Run time in hh:mm:ss
+#SBATCH --mem-per-cpu=64gb     # Maximum memory required per CPU (in megabytes)
+#SBATCH --job-name=SNP
+#SBATCH --error=SNP.%J.err
+#SBATCH --output=SNP.%J.out
+
+export MINICONDA_HOME="~/miniconda3/envs/sanva/bin/"
+export GITHUB_DIR=`pwd`
+module load R
+cd $WORK
+mkdir SNP-outputs
+cd $WORK/SNP-outputs
+mkdir trimmomatic
+cd trimmomatic
+mkdir trimlog
+cd ../
+mkdir samfiles
+mkdir bamfiles
+mkdir flagsam
+mkdir sortsam
+mkdir depth
+mkdir stats
+mkdir picard
+mkdir freebayesoutput
+cd picard
+mkdir picardlog
+cd $WORK/SNP/
+
+for x in `cat inputs.txt`; do 
+python3 pythonVariantAnalysis.py ./$x $MINICONDA_HOME $GITHUB_DIR $x
+sh
+done
