e7d5550076ee23a135cc05f56f4d58819544aab4
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-16 16:36:00 -0500

    Update SNP_process.sh

--- SNP_process.sh
+++ SNP_process.sh
@@ -5,6 +5,7 @@
 #SBATCH --error=SNP.%J.err
 #SBATCH --output=SNP.%J.out
 
+
 export MINICONDA_HOME="~/miniconda3/envs/sanva/bin/"
 export GITHUB_DIR=`pwd`
 module load R
@@ -26,6 +27,11 @@ mkdir freebayesoutput
 cd picard
 mkdir picardlog
 cd $WORK/SNP/
+mkdir length
+#-------------------- create file name
+Rscript fileName.R $GITHUB_DIR/data $GITHUB_DIR/InputFiles.csv
+split -l 10 InputFiles.csv new    
+rename -n  's/new/inputfilemm' new*
 
 for x in `cat inputs.txt`; do 
 python3 pythonVariantAnalysis.py ./$x $MINICONDA_HOME $GITHUB_DIR $x
