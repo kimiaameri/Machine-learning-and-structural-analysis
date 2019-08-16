966d217a52dc96c9ae5c50da113e08e75ceb6161
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-16 14:54:46 -0500

    Update pythonVariantAnalysis.py

--- pythonVariantAnalysis.py
+++ pythonVariantAnalysis.py
@@ -24,9 +24,11 @@ with open(outputFile,'w') as outFile:
         csv_reader = csv.reader(csv_file, delimiter=',')
         
         for row in csv_reader:
-            if count !=0:
-              outFile.write(f' cd $WORK/SNP/Length/\n' )
-              outFile.write(f'export LengthReverse=$(( `cat length_filtered_{row[0]}_2.fastq.txt` ))  \n' )
+              outFile.write(f' cd $WORK/SNP/data/\n' )
+              outFile.write(f'cat  filtered_{row[0]}_2.fastq. | grep -o "length=.*$" | cut -f2 -d'=' > ../length/{row[0]}.txt  \n' )
+              outFile.write(f' cd $WORK/SNP/length/\n' )
+              outFile.write(f' awk '{ total += $1; count++ } END { print total/count }'  {row[0]}.txt >length_{row[0]}.txt \n' )
+              outFile.write(f'export LengthReverse=$(( `cat length_{row[0]}.txt` ))  \n' )
               outFile.write("LengthReverse=${LengthReverse%.*} \n")
               outFile.write(f'{minicondaBin}trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SNP-outputs/trimmomatic/trimlog/{row[0]}.trimlog $WORK/SNP/data/filtered_{row[1]} $WORK/SNP/data/filtered_{row[2]} $WORK/SNP-outputs/trimmomatic/{row[0]}-R1.paired.fq $WORK/SNP-outputs/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/SNP-outputs/trimmomatic/{row[0]}-R2.paired.fq $WORK/SNP-outputs/trimmomatic/{row[0]}-R2.unpaired.fq SLIDINGWINDOW:4:15 MAXINFO:50:0.5 LEADING:3 TRAILING:3 MINLEN:$LengthReverse \n')
               outFile.write(f' cd $WORK/SNP-outputs/trimmomatic/\n' )
@@ -39,5 +41,4 @@ with open(outputFile,'w') as outFile:
               outFile.write(f'{minicondaBin}/samtools stats $WORK/SNP-outputs/sortsam/{row[0]}.sorted.bam >$WORK/SNP-outputs/stats/{row[0]}.txt \n')
               outFile.write(f'{minicondaBin}picard MarkDuplicates I=$WORK/SNP-outputs/sortsam/{row[0]}.sorted.bam O=$WORK/SNP-outputs/picard/{row[0]}.picard.bam M=$WORK/SNP-outputs/picard/picardlog/{row[0]}.picard.log\n')
               outFile.write(f'{minicondaBin}freebayes -f $WORK/SNP_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/WholeGenomeFasta/genome.fa $WORK/SNP-outputs/picard/{row[0]}.picard.bam >$WORK/SNP-outputs/freebayesoutput/{row[0]}.vcf\n')
-
-            count =count + 1
+              outFile.write(f'{minicondaBin}samtools depth -a $WORK/SNP-outputs/sortsam/{row[0]}.sorted.bam > $WORK/SNP-outputs/depth/{row[0]}.depth\n')
