a7f05f07d3e2d268bda09b284bc288fd2f7dff58
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-19 15:24:52 -0500

    Update pythonBCF_VCF.py

--- pythonBCF_VCF.py
+++ pythonBCF_VCF.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 
-if len(sys.argv) < 6:
+if len(sys.argv) < 5:
     sys.stderr.write('not enough input file: No Input CSV file or bcftools  or qulaity or depth \n')
     sys.exit(0)
     
@@ -9,9 +9,7 @@ inputFile = sys.argv[1]
 minicondaBin = sys.argv[2]
 quality = sys.argv[3]
 depth= sys.argv[4]
-n= sys.argv[5]
-index = n.split('.')[0][10:]
-outputFile ="BCF-VCF{}.sh".format(index)
+outputFile = "BCF-VCF.sh"
 with open(outputFile,'w') as outFile:
     outFile.write('#!/bin/sh \n')
     outFile.write('#SBATCH --time=100:00:00   # Run time in hh:mm:ss  \n')
