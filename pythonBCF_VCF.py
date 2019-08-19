a1af4072b7b4298031fa2efa3f68ee3487f0d887
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-19 12:33:14 -0500

    Update pythonBCF_VCF.py

--- pythonBCF_VCF.py
+++ pythonBCF_VCF.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 
-if len(sys.argv) < 4:
+if len(sys.argv) < 6:
     sys.stderr.write('not enough input file: No Input CSV file or bcftools  or qulaity or depth \n')
     sys.exit(0)
     
@@ -9,7 +9,9 @@ inputFile = sys.argv[1]
 minicondaBin = sys.argv[2]
 quality = sys.argv[3]
 depth= sys.argv[4]
-outputFile = "BCF-VCF.sh"
+n= sys.argv[5]
+index = n.split('.')[0][10:]
+outputFile ="BCF-VCF{}.sh".format(index)
 with open(outputFile,'w') as outFile:
     outFile.write('#!/bin/sh \n')
     outFile.write('#SBATCH --time=100:00:00   # Run time in hh:mm:ss  \n')
@@ -22,10 +24,8 @@ with open(outputFile,'w') as outFile:
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            if count !=0:
                 outFile.write(f'{minicondaBin}vcffilter -f "QUAL >{quality}" $WORK/SNP-outputs/freebayesoutput/{row[0]}.vcf >$WORK/SNP-outputs/vcffilter-q/{row[0]}.vcf\n')
                 outFile.write(f'{minicondaBin}vcffilter -f "DP > {depth}" $WORK/SNP-outputs/vcffilter-q/{row[0]}.vcf > $WORK/SNP-outputs/vcffilter-q-dp/{row[0]}.vcf\n')
                 outFile.write(f'{minicondaBin}bcftools view -Ob $WORK/SNP-outputs/vcffilter-q-dp/{row[0]}.vcf > $WORK/SNP-outputs/bcfoutput/{row[0]}.vcf.gz\n')
                 outFile.write(f'{minicondaBin}bcftools index $WORK/SNP-outputs/bcfoutput/{row[0]}.vcf.gz\n')
-            count =count + 1
- 
+
