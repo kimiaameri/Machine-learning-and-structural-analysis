f719d917e518fb3191c0e2f57859337084b8285e
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-20 12:32:35 -0500

    Update pythonsnpEff.py

--- pythonsnpEff.py
+++ pythonsnpEff.py
@@ -18,14 +18,14 @@ with open(outputFile,'w') as outFile:
     outFile.write('#SBATCH --job-name=snpEff \n')
     outFile.write('#SBATCH --error=snpEff.%J.err \n')
     outFile.write('#SBATCH --output=snpEff.%J.out \n')  
-    outFile.write('sed -i \'s/^chr/Chromosome/\' $WORK/SNP-outputs/vcffilter-q-dp/*.vcf;\n')
+    #outFile.write('sed -i \'s/^chr/Chromosome/\' $WORK/SNP-outputs/vcffilter-q-dp/*.vcf;\n')
 
     count=0
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            outFile.write(f'{minicondaBin}snpEff -v Staphylococcus_aureus_subsp_aureus_nctc_8325 $WORK/SNP-outputs/vcffilter-q-dp/{row[0]}.vcf > $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf \n')
-            outFile.write(f'mv $WORK/SNP/snpEff_genes.txt $WORK/SNP-outputs/snpEff/snpEff-gene/{row[0]}.txt \n')
-            outFile.write(f'mv $WORK/SNP/snpEff_summary.html $WORK/SNP-outputs/snpEff/snpEff-summary/{row[0]}.html \n')
-            #filter_variant = "(Cases[0] = 3) & (Controls[0] = 0) & ((ANN[*].IMPACT = 'HIGH') | (ANN[*].IMPACT = 'MODERATE'))"
-            #outFile.write(f'cat $WORK/SNP-outputs/snpEff/{row[0].ann.vcf | {minicondaBin}SnpSift.jar filter {filter_variant} > $WORK/SNP-outputs/snpEff/snpEff-filtered/{row[0]}.filtered.vcf \n')
+            #outFile.write(f'{minicondaBin}snpEff -v Staphylococcus_aureus_subsp_aureus_nctc_8325 $WORK/SNP-outputs/vcffilter-q-dp/{row[0]}.vcf > $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf \n')
+            #outFile.write(f'mv $WORK/SNP/snpEff_genes.txt $WORK/SNP-outputs/snpEff/snpEff-gene/{row[0]}.txt \n')
+            #outFile.write(f'mv $WORK/SNP/snpEff_summary.html $WORK/SNP-outputs/snpEff/snpEff-summary/{row[0]}.html \n')
+            filter_variant = "(Cases[0] = 3) & (Controls[0] = 0) & ((ANN[*].IMPACT = 'HIGH') | (ANN[*].IMPACT = 'MODERATE'))"
+            outFile.write(f'cat $WORK/SNP-outputs/snpEff/{row[0].ann.vcf | {minicondaBin}SnpSift.jar filter {filter_variant} > $WORK/SNP-outputs/snpEff/snpEff-filtered/{row[0]}.filtered.vcf \n')
