2ab1e671834a8bb030795de1b3c8a449bacad68d
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-21 12:11:12 -0500

    Update installTools.sh

--- installTools.sh
+++ installTools.sh
@@ -1,18 +1,9 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
+conda env create -f sanva.environment.yaml
+conda activate
+conda activate sanva
 
-
-conda create -n stampy
-conda activate stampy
-conda install python=2.7
-
-conda env create -f stampy.environment.yaml
-
-cd $WORK/SNP-sonfwares
-wget https://005.medsci.ox.ac.uk/files-library/stampy-latest.tgz 
-tar zxvf stampy-latest.tgz
-rm stampy-latest.tgz
-cd stampy-1.0.32 
-make python=python2.7
 
 
