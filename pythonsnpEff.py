bbdfb3a4b019b4fecb2c87996f0d60c776e4877a
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-22 10:46:30 -0500

    Update pythonsnpEff.py

--- pythonsnpEff.py
+++ pythonsnpEff.py
@@ -1,15 +1,16 @@
 import csv
 import sys
 
-if len(sys.argv) < 4:
+if len(sys.argv) < 3:
     sys.stderr.write('No Input CSV file and samtools\n')
     sys.exit(0)
     
 inputFile = sys.argv[1]
 minicondaBin = sys.argv[2]
-n= sys.argv[3]
-index = n.split('.')[0][10:]
-outputFile ="snpEff{}.sh".format(index)
+#n= sys.argv[3]
+#index = n.split('.')[0][10:]
+#outputFile ="snpEff{}.sh".format(index)
+outputFile ="snpEff.sh"
 with open(outputFile,'w') as outFile:
     outFile.write('#!/bin/sh \n')
     outFile.write('#SBATCH --time=100:00:00   # Run time in hh:mm:ss  \n')
@@ -18,14 +19,18 @@ with open(outputFile,'w') as outFile:
     outFile.write('#SBATCH --job-name=snpEff \n')
     outFile.write('#SBATCH --error=snpEff.%J.err \n')
     outFile.write('#SBATCH --output=snpEff.%J.out \n')  
-    #outFile.write('sed -i \'s/^chr/Chromosome/\' $WORK/SNP-outputs/vcffilter-q-dp/*.vcf;\n')
+    outFile.write('sed -i \'s/^chr/Chromosome/\' $WORK/SNP-outputs/vcffilter-q-dp/*.vcf;\n')
 
     count=0
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            #outFile.write(f'{minicondaBin}snpEff -v Staphylococcus_aureus_subsp_aureus_nctc_8325 $WORK/SNP-outputs/vcffilter-q-dp/{row[0]}.vcf > $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf \n')
-            #outFile.write(f'mv $WORK/SNP/snpEff_genes.txt $WORK/SNP-outputs/snpEff/snpEff-gene/{row[0]}.txt \n')
-            #outFile.write(f'mv $WORK/SNP/snpEff_summary.html $WORK/SNP-outputs/snpEff/snpEff-summary/{row[0]}.html \n')
-            filter_variant = "(Cases[0] = 3) & (Controls[0] = 0) & ((ANN[*].IMPACT = 'HIGH') | (ANN[*].IMPACT = 'MODERATE'))"
-            outFile.write(f'cat $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf | {minicondaBin}SnpSift.jar filter {filter_variant} > $WORK/SNP-outputs/snpEff/snpEff-filtered/{row[0]}.filtered.vcf \n')
+          if count !=0 :
+            outFile.write(f'{minicondaBin}snpEff -v Staphylococcus_aureus_subsp_aureus_nctc_8325 $WORK/SNP-outputs/vcffilter-q-dp/{row[0]}.vcf > $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf \n')
+            outFile.write(f'mv $WORK/SNP/snpEff_genes.txt $WORK/SNP-outputs/snpEff/snpEff-gene/{row[0]}.txt \n')
+            outFile.write(f'mv $WORK/SNP/snpEff_summary.html $WORK/SNP-outputs/snpEff/snpEff-summary/{row[0]}.html \n')
+            #filter_variant = "(Cases[0] = 3) & (Controls[0] = 0) & ((ANN[*].IMPACT = 'HIGH') | (ANN[*].IMPACT = 'MODERATE'))"
+            #outFile.write(f'cat $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf | $WORK/SAEVA_softwares/snpEff/SnpSift.jar filter "TYPE=snp" > $WORK/SNP-outputs/snpEff/snpEff-filtered/{row[0]}.filtered.vcf \n')
+            #outFile.write(f'cat $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf | java -jar $WORK/SAEVA_softwares/snpEff/SnpSift.jar filter \ "TYPE=snp"\ > $WORK/SNP-outputs/snpEff/snpEff-filtered/{row[0]}.filtered.vcf \n')
+
+          count =count +1
