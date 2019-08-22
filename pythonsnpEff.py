8867513288c04b8b77933efe39a99eea218bdf0d
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-22 17:55:45 -0500

    Update pythonsnpEff.py

--- pythonsnpEff.py
+++ pythonsnpEff.py
@@ -29,6 +29,8 @@ with open(outputFile,'w') as outFile:
             outFile.write(f'{minicondaBin}snpEff -v Staphylococcus_aureus_subsp_aureus_nctc_8325 $WORK/SNP-outputs/vcffilter-q-dp/{row[0]}.vcf > $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf \n')
             outFile.write(f'mv $WORK/SNP/snpEff_genes.txt $WORK/SNP-outputs/snpEff/snpEff-gene/{row[0]}.txt \n')
             outFile.write(f'mv $WORK/SNP/snpEff_summary.html $WORK/SNP-outputs/snpEff/snpEff-summary/{row[0]}.html \n')
+            outFile.write(f'cat $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf | {minicondaBin}SnpSift.jar filter \ "(TYPE[*] has 'snp')"\ > $WORK/SNP-outputs/snpEff/filtered/{row[0]}.filtered.vcf \n')
+
             #filter_variant = "(Cases[0] = 3) & (Controls[0] = 0) & ((ANN[*].IMPACT = 'HIGH') | (ANN[*].IMPACT = 'MODERATE'))"
             #outFile.write(f'cat $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf | $WORK/SAEVA_softwares/snpEff/SnpSift.jar filter "TYPE=snp" > $WORK/SNP-outputs/snpEff/snpEff-filtered/{row[0]}.filtered.vcf \n')
             #outFile.write(f'cat $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf | java -jar $WORK/SAEVA_softwares/snpEff/SnpSift.jar filter \ "TYPE=snp"\ > $WORK/SNP-outputs/snpEff/snpEff-filtered/{row[0]}.filtered.vcf \n')
