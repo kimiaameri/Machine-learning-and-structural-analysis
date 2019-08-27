05c63c0b813ece3781bb235a3b72da88761ff3f0
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-27 18:26:15 -0500

    Update installTools.sh

--- installTools.sh
+++ installTools.sh
@@ -5,6 +5,7 @@ if [ -z $WORK ]; then WORK=`pwd`; fi
 cd 
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
 cd $WORK/SNP
 conda env create -f snpvariant.environment.yaml
 conda activate
