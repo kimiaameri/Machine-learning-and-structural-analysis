3fd96b72559b811e87c5a4a96f2306a764a60964
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-31 17:16:02 -0500

    Update fileName.R

--- fileName.R
+++ fileName.R
@@ -5,22 +5,31 @@ OutputFile <- argv[2]
 
 listdata<- list.files(InputPath)
 #listdata<- gsub(pattern = "filtered_",replacement = "",listdata, perl = T)
-z1<- gsub(pattern = '_([1-9]+).*',replacement = "",listdata, perl = T)
+#z1<- gsub(pattern = '_([1-9]+).*',replacement = "",listdata, perl = T)
 
-z<-unique(z1)
-inputFile<- matrix(NA, ncol=3, nrow=length(z))
-inputFile[,1]<- z
+#z<-unique(z1)
+#inputFile<- matrix(NA, ncol=3, nrow=length(z))
+#inputFile[,1]<- z
 #listdata<- unique(listdata)
-even_indexes<-seq(2,length(listdata),2)
-odd_indexes<-seq(1,length(listdata),2)
+#even_indexes<-seq(2,length(listdata),2)
+#odd_indexes<-seq(1,length(listdata),2)
 
-for (i in 1:length(z))
-{
-  if ( i %% 2== 1) {inputFile[i,2]<- as.matrix(as.character(listdata[odd_indexes[i]]))
-  inputFile[i,3]<- as.matrix(as.character(listdata[even_indexes[i]]))}
-  
-  if ( i %% 2== 0) {inputFile[i,2]<- as.matrix(as.character(listdata[odd_indexes[i]]))
-  inputFile[i,3]<- as.matrix(as.character(listdata[even_indexes[i]]))}
-}
-colnames(inputFile)<- c("SrA.Accession","Forward reads", "Reverse Reads")
+#for (i in 1:length(z))
+#{
+ # if ( i %% 2== 1) {inputFile[i,2]<- as.matrix(as.character(listdata[odd_indexes[i]]))
+ # inputFile[i,3]<- as.matrix(as.character(listdata[even_indexes[i]]))}
+ # 
+ # if ( i %% 2== 0) {inputFile[i,2]<- as.matrix(as.character(listdata[odd_indexes[i]]))
+ # inputFile[i,3]<- as.matrix(as.character(listdata[even_indexes[i]]))}
+#}
+#colnames(inputFile)<- c("SrA.Accession","Forward reads", "Reverse Reads")
+#write.csv(inputFile, OutputFile, row.names = F)
+listdata<- gsub(pattern = '_([1-9]+).*',replacement = "",listdata, perl = T)
+listdata<- unique(listdata)
+
+inputFile<-as.matrix(listdata) 
+c1<- as.matrix(paste(as.character(inputFile[,1]),"_1.fastq", sep=""))
+c2<- as.matrix(paste(as.character(inputFile[,1]),"_2.fastq", sep=""))
+inputFile<- cbind(inputFile, c1, c2)
+colnames(inputFile)<- c("name","Forward reads", "Reverse Reads")
 write.csv(inputFile, OutputFile, row.names = F)
