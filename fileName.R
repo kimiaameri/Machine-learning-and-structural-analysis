08d48f3b13c749767ba945393e12f3b5fb97649e
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-08-31 17:27:24 -0500

    Update fileName.R

--- fileName.R
+++ fileName.R
@@ -4,16 +4,16 @@ OutputFile <- argv[2]
 
 
 listdata<- list.files(InputPath)
-#listdata<- gsub(pattern = "filtered_",replacement = "",listdata, perl = T)
-listdata<- gsub(pattern = '_([1-9]+).*',replacement = "",listdata, perl = T)
-z<- unique(listdata)
+z<- gsub(pattern = '_([1-9]+).*',replacement = "",listdata, perl = T)
 
-inputFile<- matrix(NA, ncol=3, nrow=length(z))
-inputFile[,1]<- z
+z1<-unique(z)
+inputFile<- matrix(NA, ncol=3, nrow=length(z1))
+inputFile[,1]<- z1
+#listdata<- unique(listdata)
 even_indexes<-seq(2,length(listdata),2)
 odd_indexes<-seq(1,length(listdata),2)
 
-for (i in 1:length(z))
+for (i in 1:length(z1))
 {
   if ( i %% 2== 1) {inputFile[i,2]<- as.matrix(as.character(listdata[odd_indexes[i]]))
   inputFile[i,3]<- as.matrix(as.character(listdata[even_indexes[i]]))}
