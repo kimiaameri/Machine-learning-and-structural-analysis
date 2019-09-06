7de91fb8de93858f7ebd735ec7e471531dea745a
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-09-06 14:45:22 -0500

    Update pythonsnpEff.py

--- pythonsnpEff.py
+++ pythonsnpEff.py
@@ -1,36 +1,50 @@
 import csv
 import sys
 
-if len(sys.argv) < 3:
-    sys.stderr.write('No Input CSV file and samtools\n')
+if len(sys.argv) < 4:
+    sys.stderr.write('No Input CSV file and BCFTools and java\n')
     sys.exit(0)
     
 inputFile = sys.argv[1]
 minicondaBin = sys.argv[2]
-#n= sys.argv[3]
-#index = n.split('.')[0][10:]
-#outputFile ="snpEff{}.sh".format(index)
-outputFile ="snpEff.sh"
+cpath = sys.argv[3]
+outputFile = "snpEffMerge.sh"
+prefix = "$WORK/SNP-outputs/bcfoutput/"
+count=0
+with open(inputFile) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    allSamples=[]
+    for row in csv_reader:
+        if count !=0 :
+          allSamples.append(row[0])
+        count =count +1
+    listAll=[]
+    for sample in allSamples :
+        listAll.append(prefix + sample + ".vcf.gz")
+        
+    allStr  = ' '.join(listAll) 
+    with open (cpath +"/allSamples.txt",'w') as of:
+         of.write(str(allSamples))
+    length=len(allSamples)
 with open(outputFile,'w') as outFile:
-    outFile.write('#!/bin/sh \n')
-    outFile.write('#SBATCH --time=100:00:00   # Run time in hh:mm:ss  \n')
-    outFile.write('#SBATCH --mem-per-cpu=64gb  \n')
-    # Maximum memory required per CPU (in megabytes')
-    outFile.write('#SBATCH --job-name=snpEff \n')
-    outFile.write('#SBATCH --error=snpEff.%J.err \n')
-    outFile.write('#SBATCH --output=snpEff.%J.out \n')  
-    outFile.write('sed -i \'s/^chr/Chromosome/\' $WORK/SNP-outputs/vcffilter-q-dp/*.vcf;\n')
+        outFile.write(f'{minicondaBin}bcftools merge --force {allStr} -O v -o $WORK/SNP-outputs/mergefile.vcf;\n')
+        outFile.write('sed -i \'s/^chr/Chromosome/\' $WORK/SNP-outputs/mergefile.vcf;\n')
+        outFile.write(f'{minicondaBin}snpEff -v Staphylococcus_aureus_subsp_aureus_nctc_8325 $WORK/SNP-outputs/mergefile.vcf > $WORK/SNP-outputs/merge_snpEff.ann.vcf \n')
+        outFile.write('mv $WORK/SNP/snpEff_genes.txt $WORK/SNP-outputs/merge_snpEff.txt \n')
+        outFile.write('mv $WORK/SNP/snpEff_summary.html $WORK/SNP-outputs/merge_snpEff_summary.html \n')
+        outFile.write('sed -i \'s/^chr/Chromosome/\' $WORK/SNP-outputs/vcffilter-dp/*.vcf;\n')
 
-    count=0
-    with open(inputFile) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-          if count !=0 :
-            outFile.write("cd $WORK/SNP/ \n")
-            outFile.write(f'{minicondaBin}snpEff -v Staphylococcus_aureus_subsp_aureus_nctc_8325 $WORK/SNP-outputs/vcffilter-q-dp/{row[0]}.vcf > $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf \n')
-            outFile.write(f'mv $WORK/SNP/snpEff_genes.txt $WORK/SNP-outputs/snpEff/snpEff-gene/{row[0]}.txt \n')
-            outFile.write(f'mv $WORK/SNP/snpEff_summary.html $WORK/SNP-outputs/snpEff/snpEff-summary/{row[0]}.html \n')
-            outFile.write("cd $WORK/SNP-outputs/snpEff/ \n")
-            filter_variant = "(TYPE[*] has 'snp')"
-            outFile.write(f'cat $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf | {minicondaBin}SnpSift filter  "{filter_variant}"  > $WORK/SNP-outputs/snpEff/filtered/{row[0]}.filtered.vcf \n')
-          count =count +1
+        with open(inputFile) as csv_file:
+          csv_reader = csv.reader(csv_file, delimiter=',')
+          for row in csv_reader:
+            if count !=0 :
+              outFile.write("cd $WORK/SNP/ \n")
+              outFile.write(f'{minicondaBin}snpEff -v Staphylococcus_aureus_subsp_aureus_nctc_8325 $WORK/SNP-outputs/vcffilter-dp/{row[0]}.vcf > $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf \n')
+              outFile.write(f'mv $WORK/SNP/snpEff_genes.txt $WORK/SNP-outputs/snpEff/snpEff-gene/{row[0]}.txt \n')
+              outFile.write(f'mv $WORK/SNP/snpEff_summary.html $WORK/SNP-outputs/snpEff/snpEff-summary/{row[0]}.html \n')
+              outFile.write("cd $WORK/SNP-outputs/snpEff/ \n")
+              filter_variant = "(TYPE[*] has 'snp')"
+              outFile.write(f'cat $WORK/SNP-outputs/snpEff/{row[0]}.ann.vcf | {minicondaBin}SnpSift filter  "{filter_variant}"  > $WORK/SNP-outputs/snpEff/filtered/{row[0]}.filtered.vcf \n')
+              outFile.write(f'cat $WORK/SNP-outputs/snpEff/filtered/{row[0]}.filtered.vcf | {minicondaBin}SnpSift filter   " ( QUAL >= 500 )"   > $WORK/SNP-outputs/snpEff/filtered/quality-{row[0]}.filtered.vcf \n')
+
+            count =count +1
