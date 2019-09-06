b42236bff470406ad87e0e782684e7a04cfc0692
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-09-06 14:49:19 -0500

    Update SNP_process.sh

--- SNP_process.sh
+++ SNP_process.sh
@@ -35,5 +35,6 @@ cd $WORK/SNP-outputs/snpEff
 for x in *.vcf; do  cat $x | grep -v '##'| sed 's/AB=.*;TYPE=/TYPE=/' > $WORK/SNP-outputs/snpEff/filtered/$x; done
 find . -name "*.csv" -size 1k -delete
 ###############               merge all files together,remove headers and sort based on positions      #########
-for x in *.vcf; do  cat $x | sort -k2,2 | grep -v '##'| grep -v '#' >$WORK/SNP-outputs/snpEff/filtered/2/filtered2/1/$x;done
-for x in *.filtered.vcf; do cat $x |cut -f 1 -d ":"  > /work/biocore/kimia/SNP-outputs/snpEff/filtered/2/filtered2/1/$x; done
+cd $WORK/SNP-outputs/snpEff/filtered/
+for x in *.vcf; do  cat $x | sort -k2,2 | grep -v '##' >$WORK/SNP-outputs/snpEff/filtered/$x;done
+for x in quality*.filtered.vcf; do cat $x |cut -f 1 -d ":"  > /work/biocore/kimia/SNP-outputs/snpEff/filtered/$x; done
