ed03f8c02b2ef4bd080bfda3a0e5dcdc7d8b0d8e
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-09-26 02:32:22 -0500

    Update SNP_Matrix.R

--- SNP_Matrix.R
+++ SNP_Matrix.R
@@ -1,22 +1,23 @@
 
 argv <- commandArgs(trailingOnly = TRUE)
-MergePath <- argv[1]
+#MergePath <- argv[1]
+pospath<- argv[1]
 SnpEffPath <- argv[2]
 SNPcoreMatrix <- argv[3]
-SNP1<- read.csv(paste0(MergePath,"clean_merge_snpEff1.ann.vcf"), header = TRUE, sep="\t",stringsAsFactors = F)
-SNP2<- read.csv(paste0(MergePath,"clean_merge_snpEff2.ann.vcf"), header = TRUE, sep="\t",stringsAsFactors = F)
-SNP3<- read.csv(paste0(MergePath,"clean_merge_snpEff3.ann.vcf"), header = TRUE, sep="\t",stringsAsFactors = F)
-SNP3<- SNP3[,1:5] 
+#SNP1<- read.csv(paste0(MergePath,"clean_merge_snpEff1.ann.vcf"), header = TRUE, sep="\t",stringsAsFactors = F)
+#SNP2<- read.csv(paste0(MergePath,"clean_merge_snpEff2.ann.vcf"), header = TRUE, sep="\t",stringsAsFactors = F)
+#SNP3<- read.csv(paste0(MergePath,"clean_merge_snpEff3.ann.vcf"), header = TRUE, sep="\t",stringsAsFactors = F)
+#SNP3<- SNP3[,1:5] 
 
-SNPa.Matrix<- rbind(SNP1,SNP2,SNP3)
-listpos<- unique(SNPa.Matrix$POS)
+#SNPa.Matrix<- rbind(SNP1,SNP2,SNP3)
+listpos<- read.csv(paste0(Pospath,"uniq.pos.txt"), header = False, sep="\t",stringsAsFactors = F)
 
 #-----------------------
 listsnp <- list.files(SnpEffPath)
 l<-length(listsnp)
 
-SNP.Matrix <- matrix(0,nrow=length(listpos),ncol=l)
-listname<- gsub("cut_filtered",replacement = "",listsnp,  perl = T)
+SNP.Matrix <- matrix(0,nrow=nrow (listpos),ncol=l)
+listname<- gsub("filtered",replacement = "",listsnp,  perl = T)
 listnames<- gsub(".ann.vcf",replacement = "",listname,  perl = T)
 colnames(SNP.Matrix) <-  listnames 
 rownames(SNP.Matrix) <- listpos
